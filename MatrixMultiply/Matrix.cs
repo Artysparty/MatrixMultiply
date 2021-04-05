08744eace4c050ca84743d3fe8a59776bec910db
Falkonny <62031372+Falkonny@users.noreply.github.com>
2021-04-05 10:45:18 +0300

    Добавьте файлы проекта.

---
+++ MatrixMultiply/Matrix.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace MatrixMultiply
+{
+    internal class Matrix
+    {
+        public int Size { get; set; }
+        private readonly int[,] _data;
+
+
+        public Matrix(int size)
+        {
+            Size = size;
+            _data = new int[size, size];
+        }
+
+        public int this[int x, int y]
+        {
+            get => _data[x, y];
+            set => _data[x, y] = value;
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            for (var i = 0; i < Size; i++)
+            {
+                stringBuilder.Append("[ ");
+                for (var j = 0; j < Size; j++)
+                {
+                    stringBuilder.Append(_data[i, j]);
+                    stringBuilder.Append(j == Size - 1 ? " " : ", ");
+                }
+
+                stringBuilder.AppendLine("]");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
